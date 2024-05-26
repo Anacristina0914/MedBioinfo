fb152c223fb7ea50caf4f2468234771c67ca02a5
Anacristina0914 <anacristina0914@gmail.com>
2024-05-26 18:22:46 +0200

    Modified end of script to print date at the end

--- scripts/x_anago_read_QC.sh
+++ scripts/x_anago_read_QC.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "script start: download and initial sequencing read quality control"
-date
-echo first version
-date
-echo "script end."
+echo "script start`date`: Creating file with accession numbers..."
+# Extract run accession numbers from db
+sqlite3 -noheader -csv -batch /proj/applied_bioinformatics/common_data/sample_collab.db \
+	"select run_accession from sample_annot spl left join sample2bioinformatician s2b using(patient_code) where username='x_anago';" > \
+	/proj/applied_bioinformatics/users/x_anago/MedBioinfo/analyses/x_anago_run_accessions.txt 
+
+echo "script end: `date`"
